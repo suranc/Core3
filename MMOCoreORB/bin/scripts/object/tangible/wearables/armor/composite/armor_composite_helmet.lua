345a39f44b64f2fd15a18fdbe6ed542ac4f50963
Chris Suran <suran.c@gmail.com>
2019-04-15 01:53:08 -0400

    Fixing stun special resist
    Change-Id: I88aae63ebac490a7c2f16252af18789e4c361335

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_helmet.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_helmet.lua
@@ -86,7 +86,7 @@ object_tangible_wearables_armor_composite_armor_composite_helmet = object_tangib
 	-- Damage types in WeaponObject
 	vulnerability = LIGHTSABER,
 
-	specialResists = ELECTRICITY, STUN
+	specialResists = ELECTRICITY + STUN,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 0,
--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_leggings.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/composite/armor_composite_leggings.lua
@@ -93,7 +93,7 @@ object_tangible_wearables_armor_composite_armor_composite_leggings = object_tang
 	-- Damage types in WeaponObject
 	vulnerability = LIGHTSABER,
 
-	specialResists = ELECTRICITY, STUN
+	specialResists = ELECTRICITY + STUN,
 
 	-- These are default Blue Frog stats
 	healthEncumbrance = 0,
